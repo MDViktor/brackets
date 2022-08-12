78d30a37b7ad1582a251e18497c4a9bab6a647dd
MDViktor <milliondollarboymvd@gmail.com>
2022-08-13 01:34:03 +0300

    feat: second  probe

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // if (str.length%2!==0){
-  //   // console.log(str,str.length);
-  //   return false;
-  // }
-
   console.log(str);
-  console.log(bracketsConfig);
-  // for (i=0;i<str.length;i++){
-  //   if (str[str.length-1]==='('||str[str.length-1]==='['||str[str.length-1]==='{'){
-  //     return false;
-  //   }
-  // }
   const obj = {};
   let stack = [];
   for (const key of bracketsConfig) {
-    obj[key[0]] = key[1];
+    obj[key[1]] = key[0];
   }
+  console.log(obj);
   for (i=0;i<str.length;i++){
     let curr = str[i];
     if (curr === '('||curr === '{'||curr === '['||curr === '|'){
       stack.push(curr);
-      console.log(curr);
+      console.log(stack.length);
     } else {
       if (stack.length === 0){
         return false;
       }
       let last = stack[stack.length - 1];
       console.log(last);
+      
+      
+      console.log(obj[curr]);
       if (obj[curr] === last){
         stack.pop();
       }
@@ -35,8 +28,6 @@ module.exports = function check(str, bracketsConfig) {
       }
     }
   }
-
-    // console.log(str.split(''));
-  return stack.length === 0;
-  
+  console.log('this', stack.length)
+  return stack.length === 0; 
 }
